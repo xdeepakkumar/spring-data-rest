93a359cf8949171b9ebcd00dd4400f2eba643e31
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-10-25 19:37:03 +0530

    changing the path and path name using the @RepositoryRestResource

--- src/main/java/com/springdatarest/repository/UserRepository.java
+++ src/main/java/com/springdatarest/repository/UserRepository.java
@@ -1,11 +1,13 @@
 package com.springdatarest.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
 import com.springdatarest.entity.User;
 
 @Repository
+@RepositoryRestResource(path = "employees")
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
