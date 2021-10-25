5c03dbe3339b731e6787b241fd1965fd1a4560f2
xdeepakkumar <xdeepakkumar355@gmail.com>
2021-10-25 19:52:58 +0530

    adding a query method for searching a user by findByFirstName

--- src/main/java/com/springdatarest/repository/UserRepository.java
+++ src/main/java/com/springdatarest/repository/UserRepository.java
@@ -1,6 +1,9 @@
 package com.springdatarest.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
@@ -9,5 +12,6 @@ import com.springdatarest.entity.User;
 @Repository
 @RepositoryRestResource(path = "employees")
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	
+	List<User> findByFirstName(@Param("firstName") String firstNameString);
 }
